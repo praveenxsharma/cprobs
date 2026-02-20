dcb4647ccccc9fa24c02dd4aa34db26555a59f8d
praveenxsharma <itachisec@duck.com>
2026-02-20 12:01:23 +0530

    added upto 20/02/2026

---
+++ class/employee.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+int Salary;
+  
+
+  cout << "Enter Monthly Salary: \n" ; 
+  cin >> Salary ;
+
+  int Annual_Salary ;
+  Annual_Salary=Salary*12 ;
+
+  cout << Annual_Salary << endl;
+
+  return 0;
+}
